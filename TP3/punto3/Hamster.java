553e28c4426adb1bec67c1fb752cb1d1653abbcf
Juan <juan.ramirez@est.fi.uncoma.edu.ar>
2024-09-10 13:57:45 -0300

    clase 10/9 corregido

--- TP3/punto3/Hamster.java
+++ TP3/punto3/Hamster.java
@@ -1,25 +1,37 @@
 package TP3.punto3;
 
-public class Hamster implements Runnable{
+import java.util.Random;
+
+public class Hamster implements Runnable {
     private Rueda rueda;
     private Hamaca hamaca;
     private Plato plato;
-    public Hamster (Plato unP, Hamaca unH, Rueda unaR){
-        this.plato=unP;
-        this.hamaca=unH;
-        this.rueda=unaR;
+
+    public Hamster(Plato unP, Hamaca unH, Rueda unaR) {
+        this.plato = unP;
+        this.hamaca = unH;
+        this.rueda = unaR;
     }
 
-    
     public void run() {
-        try {
-            while(true){
-                rueda.usarRueda();
-                hamaca.usarHamaca();
-                plato.usarPlato();
+        while (true) {
+            Random ran = new Random();
+            int numero = ran.nextInt(3) + 1;
+            try {
+                switch (numero) {
+                    case 1:
+                        plato.usarPlato();
+                        break;
+                    case 2:
+                        rueda.usarRueda();
+                        break;
+                    case 3:
+                        hamaca.usarHamaca();
+                        break;
+                }
+            } catch (Exception e) {
+
             }
-        } catch (Exception e) {
-            
         }
     }
 }
